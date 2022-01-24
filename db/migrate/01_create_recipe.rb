3097b26b216150cf5352fb4650f25cb4313f1ecf
luis leon <leonluis380@yahoo.com>
2022-01-23 20:57:16 -0500

    Done.

---
+++ db/migrate/01_create_recipe.rb
@@ -0,0 +1,12 @@
+class CreateRecipe <ActiveRecord::Migration[5.1]
+
+    def change 
+        create_table :recipes do |t|
+            t.string :name
+            t.string :ingredients
+            t.string :cook_time
+        end
+    end
+
+
+end
